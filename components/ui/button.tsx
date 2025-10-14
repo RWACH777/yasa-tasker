dd22e8e810fa60f941daf06bfa6fc20eaa363c67
rwachyeshua@gmail.com <rwachyeshua@gmail.com>
2025-10-14 16:34:32 +0000

    Fix missing backticks in button and input components

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,11 +1,13 @@
-import * as React from "react";
+"use client";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+}
 
 export function Button({ className = "", ...props }: ButtonProps) {
   return (
     <button
-      className={px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700 transition ${className}}
+      className={`px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700 transition ${className}`}
       {...props}
     />
   );
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,11 +1,13 @@
-import * as React from "react";
+"use client";
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+}
 
 export function Input({ className = "", ...props }: InputProps) {
   return (
     <input
-      className={w-full px-3 py-2 rounded-lg bg-gray-800 text-white border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}}
+      className={`w-full px-3 py-2 rounded-lg bg-gray-800 text-white border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
       {...props}
     />
   );
